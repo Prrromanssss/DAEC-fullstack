661d6d1efa9581d4788b1d55a80b2e0f80b92bbe
Sourr_cream <roman_boyko.48@mail.ru>
2024-02-09 17:22:57 +0300

    query to database to work with tables

---
+++ backend/sql/queries/users.sql
@@ -0,0 +1,9 @@
+-- name: CreateUser :one
+INSERT INTO users (id, created_at, updated_at, name, api_key)
+VALUES ($1, $2, $3, $4,
+    encode(sha256(random()::text::bytea), 'hex')
+)
+RETURNING *;
+
+-- name: GetUserByAPIkey :one
+SELECT * FROM users WHERE api_key = $1;
