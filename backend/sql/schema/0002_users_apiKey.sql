58ba426e6638224a7d795d2067f91ceb6abe6dc3
Sourr_cream <roman_boyko.48@mail.ru>
2024-02-10 13:04:05 +0300

    delete user

--- backend/sql/schema/0002_users_apiKey.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-ALTER TABLE users ADD COLUMN api_key VARCHAR(64) UNIQUE NOT NULL DEFAULT (
-    encode(sha256(random()::text::bytea), 'hex')
-);
-
--- +goose Down
-ALTER TABLE users DROP COLUMN api_key;
