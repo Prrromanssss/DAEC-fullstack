15e19446b7bd949703f583a817ee690f21ca05bd
Sourr_cream <roman_boyko.48@mail.ru>
2024-02-14 00:19:36 +0300

    create config to rabbitmq

---
+++ backend/config/rabbitmq_config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+type AMQPConfig struct {
+	Conn *amqp.Connection
+	Ch   *amqp.Channel
+}
+
+func NewConsumerAMQPConfig(amqpUrl string) *AMQPConfig {
+	conn, err := amqp.Dial(amqpUrl)
+	if err != nil {
+		log.Fatalf("Can't connect to RabbitMQ: %v", err)
+	}
+
+	log.Println("Successfully connected to RabbitMQ instance")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Can't create a channel from RabbitMQ: %v", err)
+	}
+	return &AMQPConfig{
+		Conn: conn,
+		Ch:   ch,
+	}
+}
--- backend/go.mod
+++ backend/go.mod
@@ -11,3 +11,5 @@ require (
 require github.com/google/uuid v1.6.0
 
 require github.com/lib/pq v1.10.9
+
+require github.com/streadway/amqp v1.1.0
