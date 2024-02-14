2848728cef1144e39de066f38ca08a4a36dcc318
Sourr_cream <roman_boyko.48@mail.ru>
2024-02-14 23:21:17 +0300

    config amqp consumer

--- backend/config/rabbitmq_config.go
+++ backend/config/rabbitmq_config.go
@@ -2,29 +2,76 @@ package config
 
 import (
 	"log"
+	"sync"
 
 	"github.com/streadway/amqp"
 )
 
 type AMQPConfig struct {
-	Conn *amqp.Connection
-	Ch   *amqp.Channel
+	Conn   *amqp.Connection
+	Ch     *amqp.Channel
+	Queues map[string]amqp.Queue
+	mu     *sync.Mutex
 }
 
-func NewAMQPConfig(amqpUrl string) *AMQPConfig {
+type AMQPConsumer struct {
+	Queue    amqp.Queue
+	Messages <-chan amqp.Delivery
+}
+
+func NewAMQPConfig(amqpUrl string) (*AMQPConfig, error) {
 	conn, err := amqp.Dial(amqpUrl)
 	if err != nil {
-		log.Fatalf("Can't connect to RabbitMQ: %v", err)
+		log.Printf("Can't connect to RabbitMQ: %v", err)
+		return nil, err
 	}
 
 	log.Println("Successfully connected to RabbitMQ instance")
 
 	ch, err := conn.Channel()
 	if err != nil {
-		log.Fatalf("Can't create a channel from RabbitMQ: %v", err)
+		log.Printf("Can't create a channel from RabbitMQ: %v", err)
+		return nil, err
 	}
+
 	return &AMQPConfig{
-		Conn: conn,
-		Ch:   ch,
+		Conn:   conn,
+		Ch:     ch,
+		Queues: make(map[string]amqp.Queue),
+	}, nil
+}
+
+func NewAMQPConsumer(config AMQPConfig, queueName string) (*AMQPConsumer, error) {
+	queue, err := config.Ch.QueueDeclare(
+		queueName,
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Printf("Can't create a RabbitMQ queue: %v", err)
+		return nil, err
 	}
+	config.mu.Lock()
+	config.Queues[queueName] = queue
+	config.mu.Unlock()
+	msgs, err := config.Ch.Consume(
+		queue.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Printf("Can't create a channel to consume messages from RabbitMQ: %v", err)
+		return nil, err
+	}
+	return &AMQPConsumer{
+		Queue:    queue,
+		Messages: msgs,
+	}, nil
 }
