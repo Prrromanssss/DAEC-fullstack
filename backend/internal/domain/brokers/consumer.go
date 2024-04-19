1403f6f81641421442ba5fcae30daf13aef584c6
Sourr_cream <roman_boyko.48@mail.ru>
2024-04-19 14:46:25 +0300

    update migrations

--- backend/internal/domain/brokers/consumer.go
+++
@@ -1,9 +0,0 @@
-package brokers
-
-import "github.com/streadway/amqp"
-
-// Consumer is an interface to consume messages from queue.
-type Consumer interface {
-	GetMessages() <-chan amqp.Delivery
-	Close()
-}
--- backend/internal/orchestrator/parser/get_tokens_test.go
+++
@@ -1,1 +0,0 @@
-package parser_test
